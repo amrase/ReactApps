01b4b382cd35d53d2452a76d255e64c521139d61
Amra Sezairi <amrasezairi31@gmail.com>
2020-07-21 00:39:05 -0400

    testing config file

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,7 +4,9 @@ import SearchBar from './SearchBar'
 
 class App extends React.Component{
 
-   
+    onSearchSubmit = term =>{
+        console.log(term ,'App');
+    }
 
     render() {
         return <div className="ui container" style={{marginTop:"15px"}}>
