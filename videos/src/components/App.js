9f2bf953c87263ed0c025ae68462db60bfb784a5
Amra Sezairi <amrasezairi31@gmail.com>
2020-07-21 16:00:29 -0400

    Fetched data from youtube API and create VideoList

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import apiConfig from '../config'
+import youtube from '../api/youtube'
+import VideoList from './VideoList'
+
+
 
 class App extends React.Component {
 
-    handleOnSubmit = (term) => {
-        console.log(term);
+    state  = {
+        videos : []
     }
 
+    handleOnSubmit = async (term) => {
+        const response = await youtube.get("/search",{
+            params : {
+                q : term,
+                part : 'snippet',
+                type : 'video',
+                maxResults : 5,
+                key : apiConfig.API_KEY
+            }
+        })
+
+        this.setState({
+            videos : response.data.items
+        })
+
+
+    }
+
+  
     render(){
         return <div className="ui container" style={{marginTop:'10px'}}>
             <SearchBar onSubmit = {this.handleOnSubmit}/>
+            <VideoList videos={this.state.videos}/>
         </div>
     }
 }
